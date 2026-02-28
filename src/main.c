06f8c7087a9c717dec9dffa9556f25abd51cff69
CyberOneFR <erwoan.thebault@gmail.com>
2026-02-28 10:41:59 +0000

    feat: added string view data type

--- src/main.c
+++ src/main.c
@@ -6,24 +6,26 @@
 /*   By: ethebaul <ethebaul@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/28 00:31:17 by ethebaul          #+#    #+#             */
-/*   Updated: 2026/02/28 09:55:14 by ethebaul         ###   ########.fr       */
+/*   Updated: 2026/02/28 10:40:34 by ethebaul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include "stream.h"
 
 int	main(void)
 {
 	t_stream	stream;
+	int			fd;
 	char		c;
 
 	stream_init(&stream);
-	while (stream_read(&stream, 0))
+	while (stream_read(&stream, fd))
 	{
 		while (stream_getc(&stream, &c))
-			write(1, &c, 1);
+			;
 	}
 	stream_destroy(&stream);
 	return (0);
--- src/types.h
+++ src/types.h
@@ -6,18 +6,25 @@
 /*   By: ethebaul <ethebaul@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/28 00:31:12 by ethebaul          #+#    #+#             */
-/*   Updated: 2026/02/28 09:34:25 by ethebaul         ###   ########.fr       */
+/*   Updated: 2026/02/28 10:41:23 by ethebaul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TYPES_H
 # define TYPES_H
 
+#include <cstddef>
 # include <stddef.h>
 
 typedef unsigned char	t_8b;
 typedef unsigned short	t_16b;
 typedef unsigned int	t_32b;
 typedef unsigned long	t_64b;
+
+typedef struct s_stringv
+{
+	char	*ptr;
+	size_t	size;
+}	t_stringv;
 
 #endif
